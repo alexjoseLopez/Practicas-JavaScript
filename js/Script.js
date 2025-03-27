a8ad428e95e52b53921bf1239ef4f5b2927a2e95
alexjoseLopez <alexanderlopezjose22072005@gmail.com>
2025-03-26 21:59:05 -0600

    Actualizando

--- js/Script.js
+++ js/Script.js
@@ -1,21 +1,66 @@
-function Saludar()
-{
-    alert("No saben programar");
-}
 
-function Saludar2()
-{
-    alert("mama huevo");
-}
 
 document.addEventListener("DOMContentLoaded",function()
 {
 document.getElementById("mi_boton").addEventListener("click", function()
 {
-Saludar();
-Saludar2();
+mostrar_mensaje();
+});
+});
+document.getElementById("btn_color").addEventListener("click", function()
+{
+cambiar_color();
 });
 
+function mostrar_mensaje() {
+    //Declaracion de variable
+    var datouno;
+    var datodos;
+    //Asignacion de id a las variables
 
-});
+    datouno = document.getElementById("imput1").value;
+    datodos = document.getElementById("imput2").value;
+
+    var mensaje = "Los datos son " + datouno + " " + datodos;
+     //Llamar mensaje
+
+    
+
+    if (datouno.trim() === ''|| datodos.trim() === '')
+        alert("Por favor llene sus datos");
+
+    else {        
+        document.getElementById("resultado").innerText = mensaje;
+    }
+}
+function cambiar_color(){
+    var elemento = document.getElementById("contenedor2");    
+   
+
+
+    if (document.body.style.backgroundColor === "black"){
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black"
+        elemento.style.width="200px";
+        elemento.style.height="200px";
+        elemento.style.backgroundColor="blue";
+        elemento.style.margin.left="170px";
+        elemento.style.margin.top="50px";
+        elemento.style.padding="100px";
+    }
+        
+    else{
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white"
+        elemento.style.width="150px";
+        elemento.style.height="150px";
+        elemento.style.backgroundColor="red";
+        elemento.style.margin.left="170px";
+        elemento.style.margin.top="50px";
+        elemento.style.padding="100px";
+    }
+        
+    
+}
+
 
